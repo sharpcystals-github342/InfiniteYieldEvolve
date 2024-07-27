70a585de6e505506449d5ce09538abb9875a62fa
sharpcystals-github342 <94097520+sharpcystals-github342@users.noreply.github.com>
2024-07-27 21:13:41 +1000

    Update infiniteyieldforevolve.lua

--- infiniteyieldforevolve.lua
+++ infiniteyieldforevolve.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://github.com/sharpcystals-github342/InfiniteYieldEvolve/blob/main/anticlientkick"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sharpcystals-github342/InfiniteYieldEvolve/blob/main/anticlientkick"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sharpcystals-github342/InfiniteYieldEvolve/main/boothelper.lua"))()
